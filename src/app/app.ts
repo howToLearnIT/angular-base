2f8951b67d16adb65e105cfe4f7bf84108976ddb
howToLearnIT <fifarange16@gmail.com>
2025-06-09 23:18:40 +0300

    урок 2

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,7 +2,26 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.html',
+  template: `
+    Добро пожаловать в {{ city }}.
+
+    2 + 2 = {{ 2 + 2 }} 
+  
+    {{ decision }}
+
+    {{ sayHello() }}
+  `,
   styleUrl: 'app.css'
 })
-export class App {}
+export class App {
+  city = 'Лондон';
+  isActive = false;
+
+  get decision() {
+    return this.isActive ? 'Да' : 'Нет';
+  }
+
+  sayHello() {
+    return 'Привет';
+  }
+}
