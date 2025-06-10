95aa599dd3471d11bdce75e7feea1268c135269b
howToLearnIT <fifarange16@gmail.com>
2025-06-10 11:05:38 +0300

    урок 5

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,13 +3,17 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    @if (isLoggedIn) {
-      <p>Вы авторизованы!</p>
-    } @else {
-      <p>Авторизуйтесь!</p>
+    @for (user of users; track user.id) {
+      <p>{{ user.name }}</p>
     }
   `,
 })
 export class App {
-  isLoggedIn = false;
+  users = [
+    {id: 0, name: 'Sarah'}, 
+    {id: 1, name: 'Amy'}, 
+    {id: 2, name: 'Rachel'}, 
+    {id: 3, name: 'Jessica'}, 
+    {id: 4, name: 'Poornima'},
+  ];
 }
