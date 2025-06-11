59310056f9847723849835aa1a4c3d2c0227f209
howToLearnIT <fifarange16@gmail.com>
2025-06-11 11:02:18 +0300

    урок 9

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,26 +1,34 @@
-import {Component, input} from '@angular/core';
+import {Component, input, output} from '@angular/core';
 
 @Component({
   selector: 'app-user',
   template: `
     <p>Имя пользователя: {{ name() }} и ID {{id()}} </p>
+    <button (click)="onLogout()"> Выйти </button>
   `,
 })
 export class User {
     id = input<number>(0);
     name = input<string>('username');
+    logoutEvent = output<number>();
+
+    onLogout() {
+      console.log('Выход');
+      this.logoutEvent.emit(this.id());
+    }
 }
 
 @Component({
   selector: 'app-root',
   template: `
-      <app-user [name]="name" />
-
-      <app-user name="Слава" />
-      <app-user [id]="1" />
+      <app-user [name]="name" (logoutEvent)="onLogout($event)"/>
   `,
   imports: [User],
 })
 export class App {
   name = 'Иван';
+
+  onLogout(id: number) {
+    console.log('В компоненте APP получили событие выхода от', id);
+  }
 }
