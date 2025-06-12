3755284c127bf21ab65631b220649f9ab5e72b80
howToLearnIT <fifarange16@gmail.com>
2025-06-12 10:46:28 +0300

    урок 11 после

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,25 +1,36 @@
 import {Component} from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   template: `
+    <div style="position: relative; height:300px;">
+        <img [ngSrc]="background" fill alt="Фоновая картинка" priority/>
+    </div>
+
     <p>Имя: {{ username }}</p>
     <p>Предпочительный фреймворк:</p>
     <ul>
       <li>
         Статичная картинка: <br/>
-        <img src="/assets/logo.svg" alt="Логотип Ангуляра" width="32"
+        <img 
+          ngSrc="/assets/logo.svg" 
+          alt="Логотип Ангуляра" 
+          width="32" 
+          height="32"
         />
       </li>
       <li>
         Динамическая картинка: <br/>
-        <img [src]="logoUrl" [alt]="logoAlt" width="32" />
+        <img [ngSrc]="logoUrl" [alt]="logoAlt" width="32" height="32" />
       </li>
     </ul>
   `,
+  imports: [NgOptimizedImage],
 })
 export class App {
   logoUrl = '/assets/logo.svg';
   logoAlt = 'Логотип Ангуляра';
   username = 'youngTech';
+  background = 'https://avatars.mds.yandex.net/i?id=966354b41d1bdca216b4705e31a0f2ae_l-10752166-images-thumbs&n=13'
 }
