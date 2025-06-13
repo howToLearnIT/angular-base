8a0fa01c55cb6045346e843797d6de5bcc86acbd
howToLearnIT <fifarange16@gmail.com>
2025-06-13 14:17:40 +0300

    до 15 урока

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,26 +1,22 @@
 import {Component} from '@angular/core';
-import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   template: `
-    <p>Юзер: {{ username }}</p>
-    <p>{{ username }} знает фреймворк: {{ favoriteFramework }}</p>
-
-    <label for="framework">
-      Любимый фреймворк:
-      <input id="framework" type="text" [(ngModel)]="favoriteFramework"/>
-    </label>
-
-    <button (click)="onSend()">Отправить выбор</button>
-  `,
-  imports: [FormsModule],
-})
-export class App {
-  username = 'Иван';
-  favoriteFramework = '';
+    <form>
+      <label>
+        Имя
+        <input type="text" formControlName="name" />
+      </label>
   
-  onSend() {
-    alert(this.favoriteFramework);
-  }
-}
+      <label>
+        Email
+        <input type="email" formControlName="email" />
+      </label>
+
+      <button type="submit">Отправить</button>
+    </form>
+  `,
+  imports: [],
+})
+export class App {}
