01653b0cba1de3c3d8839478e5a9452754977128
howToLearnIT <fifarange16@gmail.com>
2025-06-14 13:54:33 +0300

    урок 20 после

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
+import { ReversePipe } from './pipes';
 
 @Component({
   selector: 'app-root',
   template: `
-    Реверсивный текст: {{ text }}
+    Реверсивный текст: {{ text | reverse: 5}}
   `,
-  imports: [],
+  imports: [ReversePipe],
 })
 export class App {
   text = 'Мы почти у финиша!';
