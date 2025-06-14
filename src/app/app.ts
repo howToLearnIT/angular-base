30829e93a19c42d8e2e2b04befa373de84b4cfae
howToLearnIT <fifarange16@gmail.com>
2025-06-14 10:06:20 +0300

    урок 19 до

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,18 @@
 import {Component} from '@angular/core';
-import { LowerCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   template: `
-    {{username | lowercase}}
+    <ul>
+      <li>Число c двумя знаками после запятой {{ num }}</li>
+      <li>День рождения {{ birthday }}</li>
+      <li>Валюта {{ cost }}</li>
+    </ul>
   `,
-  imports: [LowerCasePipe]
+  imports: [],
 })
 export class App {
-  username = 'пРоГрАмМиСт ДжУнИоР';
+  num = 103.1234;
+  birthday = new Date(2023, 3, 2);
+  cost = 4560.34;
 }
