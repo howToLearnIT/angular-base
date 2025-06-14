249baf05a97d9f333a2e9fed0c5b1738207269b3
howToLearnIT <fifarange16@gmail.com>
2025-06-14 11:10:22 +0300

    урок 19 после

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,18 +1,20 @@
-import {Component} from '@angular/core';
+import {Component, LOCALE_ID} from '@angular/core';
+import {DecimalPipe, DatePipe, CurrencyPipe} from '@angular/common';
 
 @Component({
   selector: 'app-root',
   template: `
     <ul>
-      <li>Число c двумя знаками после запятой {{ num }}</li>
-      <li>День рождения {{ birthday }}</li>
-      <li>Валюта {{ cost }}</li>
+      <li>Число c двумя знаками после запятой {{ num | number: '3.2-2' }}</li>
+      <li>День рождения {{ birthday | date: 'full' : '+5' }}</li>
+      <li>Валюта {{ cost | currency: 'EUR' }}</li>
     </ul>
   `,
-  imports: [],
+  imports: [DecimalPipe, DatePipe, CurrencyPipe],
+  providers: [{ provide: LOCALE_ID, useValue: 'ru-RU' }],
 })
 export class App {
-  num = 103.1234;
+  num = 10000.1234;
   birthday = new Date(2023, 3, 2);
   cost = 4560.34;
 }
